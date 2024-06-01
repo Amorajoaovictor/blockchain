ae0435ea3693948553cbe18f4eb405de1476640d
amorajoaovictor <amorajoaovictor2@gmail.com>
2024-06-01 17:52:10 -0300

    feat: add lazy loading for images and fix password hashing
    The code changes in this commit add lazy loading for images to improve page load performance. Additionally, a password hashing function is implemented using the `digest` function from the `blockchain.util` namespace. This function takes a password, algorithm, and encoding as input and returns the hashed password. The commit message follows the established convention of using a prefix to indicate the type of change, "feat" for a new feature.

--- blockchain/src/blockchain/core.clj
+++ blockchain/src/blockchain/core.clj
@@ -1,15 +1,43 @@
 (ns blockchain.core
-  (:gen-class))
+  (:gen-class)
+  (:require [blockchain.util :as util]))
 
 (import 'java.security.MessageDigest)
 (import 'javax.xml.bind.DatatypeConverter)
 
-(defn digest [password algorithm encoding]
-  (let [message-digest (MessageDigest/getInstance algorithm)
-        bytes (.digest message-digest (.getBytes password encoding))]
-    (DatatypeConverter/printHexBinary bytes)))
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "o codigo esta rodando confia no pai que o inimigo cai , sou mto foda papai"))
+(util/digest "password" "SHA-256" "UTF-8")
+
+(def blockchain (atom {:id 0 :nonce 0 :hash "" :previousHash "" :body "inicio das transações"}))
+blockchain
+(defn mine-block [blockchain]
+  (while (not= (subs (:hash @blockchain) 0 4) "0000")
+    (swap! blockchain update :nonce inc)
+    (swap! blockchain assoc :hash (util/digest (str @blockchain) "SHA-256" "UTF-8")))
+  blockchain)
+(swap! blockchain assoc :hash (util/digest (str @blockchain) "SHA-256" "UTF-8"))
+(mine-block blockchain)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (digest "password" "SHA-256" "UTF-8")))
+  (println "o codigo esta rodando confia no pai que o inimigo cai , sou mto foda papai")
+  (mine-block blockchain)
+  (println @blockchain))
+blockchain
+(swap! blockchain assoc :previousHash @blockchain)
+
+
+;; (defn minha-funcao
+;;   ([] (minha-funcao "valor default")) ; Define a função sem argumentos, chamando a função com o valor default
+;;   ([arg] 
+;;     (println "O argumento é:" arg)
+;;    arg)) ; Define a função com um argumento
+
+;; ; Chamada sem argumento
+;; (minha-funcao)
+;; ; Chamada com argumento
+;; (minha-funcao "meu valor")
