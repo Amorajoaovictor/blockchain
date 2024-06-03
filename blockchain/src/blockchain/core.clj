ae2804167b56d0013ef21b0336e957ecc698d32f
amorajoaovictor <amorajoaovictor2@gmail.com>
2024-06-03 10:07:34 -0300

    feat: improve blockchain mining algorithm
    Refactor the `mine-block` function in `core.clj` to improve the blockchain mining algorithm. The code changes introduce a nested `mine` function that performs the mining process. It checks if the hash of the blockchain starts with "0000" and updates the blockchain's hash accordingly. This change enhances the security and reliability of the blockchain.

--- blockchain/src/blockchain/core.clj
+++ blockchain/src/blockchain/core.clj
@@ -1,7 +1,6 @@
 (ns blockchain.core
   (:gen-class)
   (:require [blockchain.util :as util]))
-
 (import 'java.security.MessageDigest)
 (import 'javax.xml.bind.DatatypeConverter)
 
@@ -12,23 +11,21 @@
 (util/digest "password" "SHA-256" "UTF-8")
 
 (def blockchain (atom {:id 0 :nonce 0 :hash "" :previousHash "" :body "inicio das transações"}))
-blockchain
-(defn mine-block [blockchain]
-  (while (not= (subs (:hash @blockchain) 0 4) "0000")
-    (swap! blockchain update :nonce inc)
-    (swap! blockchain assoc :hash (util/digest (str @blockchain) "SHA-256" "UTF-8")))
-  blockchain)
+(str @blockchain)(defn mine-block [blockchain]
+  (letfn [(mine [blockchain]
+            (let [hash (util/digest (str @blockchain) "SHA-256" "UTF-8")]
+              (if (= (subs hash 0 4) "0000")
+                (do (swap! blockchain assoc :hash hash)
+                    blockchain)
+                (do (swap! blockchain update :nonce inc)
+                    (recur blockchain)))))]
+    (mine blockchain)))
+
 (swap! blockchain assoc :hash (util/digest (str @blockchain) "SHA-256" "UTF-8"))
 (mine-block blockchain)
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "o codigo esta rodando confia no pai que o inimigo cai , sou mto foda papai")
-  (mine-block blockchain)
-  (println @blockchain))
 blockchain
-(swap! blockchain assoc :previousHash @blockchain)
+
+;; (swap! blockchain assoc :previousHash @blockchain)
 
 
 ;; (defn minha-funcao
@@ -41,3 +38,5 @@ blockchain
 ;; (minha-funcao)
 ;; ; Chamada com argumento
 ;; (minha-funcao "meu valor")
+
+
