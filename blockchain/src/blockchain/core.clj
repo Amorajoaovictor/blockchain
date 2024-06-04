0d5b1bb3cea046b24d788f7bab0b78e24e435c49
amorajoaovictor <amorajoaovictor2@gmail.com>
2024-06-04 19:59:23 -0300

    fix namespace imports

--- blockchain/src/blockchain/core.clj
+++ blockchain/src/blockchain/core.clj
@@ -1,31 +1,33 @@
 (ns blockchain.core
   (:gen-class)
-  (:require [blockchain.util :as util]))
+  (:require [blockchain.util :as util ])
+  (:require [blockchain.env :as env ]))
 (import 'java.security.MessageDigest)
 (import 'javax.xml.bind.DatatypeConverter)
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "o codigo esta rodando confia no pai que o inimigo cai , sou mto foda papai"))
+  (println "apikey:" env/apiKey))
 (util/digest "password" "SHA-256" "UTF-8")
-
 (def blockchain (atom {:id 0 :nonce 0 :hash "" :previousHash "" :body "inicio das transações"}))
-(str @blockchain)(defn mine-block [blockchain]
-  (letfn [(mine [blockchain]
-            (let [hash (util/digest (str @blockchain) "SHA-256" "UTF-8")]
+
+(str (dissoc @blockchain :hash))
+
+(defn mine-block [blockchain]
+  (letfn [(mine [block]
+            (let [hash (util/digest (str (dissoc block :hash)) "SHA-256" "UTF-8")]
               (if (= (subs hash 0 4) "0000")
                 (do (swap! blockchain assoc :hash hash)
                     blockchain)
                 (do (swap! blockchain update :nonce inc)
-                    (recur blockchain)))))]
-    (mine blockchain)))
+                    (recur @blockchain)))))]
+    (mine @blockchain)))
+
 
 (swap! blockchain assoc :hash (util/digest (str @blockchain) "SHA-256" "UTF-8"))
 (mine-block blockchain)
 blockchain
 
-;; (swap! blockchain assoc :previousHash @blockchain)
 
 
 ;; (defn minha-funcao
---
+++ blockchain/src/blockchain/env.clj
@@ -0,0 +1,3 @@
+(ns blockchain.env)
+(def apiKey "YOUR_API_key")
+(def apiUrl "https://free.currencyconverterapi.com/api/v6/convert")
